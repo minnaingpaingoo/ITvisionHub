7ca99f7af23699d6a6c1a32c7d977d3d1f5115b1
Naing Paing Oo <naingpaingoo@gmail.com>
2024-09-24 10:08:24 +0630

    Changed new method

--- Test Question/4.find-number.js
+++ Test Question/4.find-number.js
@@ -4,11 +4,15 @@
  * Find 7 is exist or not in the array
  */
 
-function find(num) {
-    var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    let length = number.length;
-    let mid = length/2;
-    console.log(mid);
+function binarySearch(arr, findingNumber, low, high) {
+    if (low > high) return -1;
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === findingNumber)
+        return mid;
+    if (arr[mid] > findingNumber)
+        return binarySearch(arr, findingNumber, low, mid - 1);
+    else
+        return binarySearch(arr, findingNumber, mid + 1, high);
 }
-
-console.log("Is 10 exist in an array? Answer is", find(10));
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+console.log("The finding number 10 is at the index of", binarySearch(number, 14, 0, number.length - 1));
